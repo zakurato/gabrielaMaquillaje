aee63635e09b8039c4bca773d790a5190fcb510b
zakurato <stiven_zeledon@hotmail.com>
2023-03-06 13:39:55 -0600

    cambios

--- resources/views/PaginaPrincipal/index.blade.php
+++ resources/views/PaginaPrincipal/index.blade.php
@@ -20,7 +20,7 @@
   </nav>
   
   <div class="image-container degradado">
-    <img src="https://www.zonadamas.mx/wp-content/uploads/2020/04/1pk-70271698_m-1.jpg" alt="Descripción de la imagen">
+    <img style="width: auto; height: auto;" src="{{URL::asset('imagenesIndex/9bbd6c3e8044d15238a62291595ffc80.jpg')}}" alt="Descripción de la imagen">
     <div class="image-text">
       <p style="font: 250% Arial">MakeuplayerGabriela</p>
     </div>
