f77017e8a33c25b882b8d5f6defbf017cce15601
zakurato <stiven_zeledon@hotmail.com>
2023-03-10 14:37:40 -0600

    cambios

--- resources/views/PaginaPrincipal/fotos.blade.php
+++ resources/views/PaginaPrincipal/fotos.blade.php
@@ -8,9 +8,6 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
     <title>Fotos</title>
 </head>
 <body>
