706888b812c3c2ed6ff62f90592bf8b34431fc18
zakurato <stiven_zeledon@hotmail.com>
2023-03-11 14:50:30 -0600

    cambios

--- resources/views/PaginaPrincipal/index.blade.php
+++ resources/views/PaginaPrincipal/index.blade.php
@@ -8,8 +8,8 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-B0+4QYYua9cJmxxnT0ZBm2HsTfVoahdDxiJ+0MlxdH1Gq3E9z1+qKTZJvAz8WfM+" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
+  integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 
   <title>Pagina Principal</title>
 </head>
@@ -37,6 +37,13 @@
     <img style="width: auto; height: auto;" src="{{URL::asset('imagenesIndex/9bbd6c3e8044d15238a62291595ffc80.jpg')}}" alt="Descripción de la imagen">
     <div class="image-text">
       <p style="font: 250% Arial">MakeuplayerGabriela</p>
+      <a href="https://www.instagram.com/makeuplayer_gabriela/" target="_blank" style="margin-right: 10px;">
+        <i class="fab fa-instagram" style="font-size: 30px; color: white"></i>
+      </a>
+      <a href="https://api.whatsapp.com/send?phone=50687249099&text=Hola!%20Quiero%20obtener%20mas%20información%20sobre%20un%20producto" target="_blank" style="margin-right: 10px;">
+        <i class="fab fa-whatsapp" style="font-size: 30px; color: white"></i>
+      </a>
+      <p class="copyri">&copy; MakeuplayerGabriela. Todos los derechos reservados. <br>
     </div>
   </div>
 
