5f1e39d78e68ce6ebbefe15c18d31e91e8f047fd
zakurato <stiven_zeledon@hotmail.com>
2023-03-11 15:53:54 -0600

    cambios

--- resources/views/PaginaPrincipal/fotos.blade.php
+++ resources/views/PaginaPrincipal/fotos.blade.php
@@ -6,20 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset("IndexCss/fotos.Css")}}">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
+    integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
     <title>Fotos</title>
-</head>
-<body>
-    <nav>
-        <ul>
-          <li><a href="{{route("index")}}">Inicio</a></li>
-          <li><a href="{{route("acercaDe")}}">Acerca de MakeuplayerGabriela</a></li>
-          <li><a href="{{route("fotos")}}">Fotos</a></li>
-          <li><a href="#">Contacto</a></li>
-          <li><a href="{{route("loginAdministracion")}}">Administración</a></li>
-        </ul>
-      </nav>
+  </head>
+  <body>
+  
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <li><a class="dropdown-item aNav" href="{{route("index")}}">Inicio</a></li>
+              <li><a class="dropdown-item aNav" href="{{route("acercaDe")}}">Acerca de MakeuplayerGabriela</a></li>
+              <li><a class="dropdown-item aNav" href="{{route("fotos")}}">Fotos</a></li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
 
       <br><br><br>
@@ -65,7 +76,16 @@
     @endforeach
     
     
-      
+    <div style="text-align: center">
+      <a href="https://www.instagram.com/makeuplayer_gabriela/" target="_blank" style="margin-right: 10px;">
+        <i class="fab fa-instagram iconosWF" style="font-size: 30px; color: black"></i>
+      </a>
+      <a href="https://api.whatsapp.com/send?phone=50687249099&text=Hola!%20Quiero%20obtener%20mas%20información%20sobre%20un%20producto" target="_blank" style="margin-right: 10px;">
+        <i class="fab fa-whatsapp iconosWF" style="font-size: 30px; color: black"></i>
+      </a>
+    </div>
+
+    <p class="copyri">&copy; MakeuplayerGabriela. Todos los derechos reservados. <br>
       
 
   
