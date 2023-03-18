3f995e0c831a832105bde8007dc134f87a224058
zakurato <stiven_zeledon@hotmail.com>
2023-03-18 17:10:48 -0600

    cambios

--- resources/views/PaginaPrincipal/index.blade.php
+++ resources/views/PaginaPrincipal/index.blade.php
@@ -369,7 +369,7 @@
     <!-- / Yoast SEO plugin. -->
 
 
-    <link rel="stylesheet" href="{{ asset('IndexCss/index.Css') }}">
+    <link rel="stylesheet" href="{{ asset('IndexCss/index.Css?1.0') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css"
         integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
