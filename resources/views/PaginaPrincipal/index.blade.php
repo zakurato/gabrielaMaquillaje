5fa11399378117e7fcb85573dd4d2ac343d02ad0
zakurato <stiven_zeledon@hotmail.com>
2023-03-18 15:53:29 -0600

    cambios

--- resources/views/PaginaPrincipal/index.blade.php
+++ resources/views/PaginaPrincipal/index.blade.php
@@ -362,7 +362,7 @@
     <meta property="og:site_name" content="Carmen Moreno">
     <meta property="article:modified_time" content="2023-01-12T11:36:17+00:00">
     <meta property="og:image"
-        content="{{asset("imagen/logo.jpg")}}">
+        content="image/logo.jpg">
     <meta property="og:image:width" content="500">
     <meta property="og:image:height" content="500">
     <meta property="og:image:type" content="image/jpeg">
