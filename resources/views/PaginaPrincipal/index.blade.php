91daf00fd4bac4337c913e0d7f18ea46c1634c89
zakurato <stiven_zeledon@hotmail.com>
2023-03-20 14:05:47 -0600

    cambios

--- resources/views/PaginaPrincipal/index.blade.php
+++ resources/views/PaginaPrincipal/index.blade.php
@@ -69,7 +69,9 @@
    
 </head>
 
-<body>
+<body
+    class="home page-template page-template-elementor_header_footer page page-id-26 elementor-default elementor-template-full-width elementor-kit-6 elementor-page elementor-page-26 e--ua-blink e--ua-chrome e--ua-webkit"
+    data-elementor-device-mode="widescreen">
 
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 0 0" width="0" height="0" focusable="false"
         role="none" style="visibility: hidden; position: absolute; left: -9999px; overflow: hidden;">
