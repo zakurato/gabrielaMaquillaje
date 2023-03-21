4572b52e9d3a3f7751ab92a96cf07b9c34996c2c
Hugo <stiven_zeledon@hotmail.com>
2023-03-20 19:03:24 -0600

    cambios

--- resources/views/PaginaPrincipal/fotos.blade.php
+++ resources/views/PaginaPrincipal/fotos.blade.php
@@ -29,7 +29,6 @@
         <!-- / Yoast SEO plugin. -->
     
     
-        <link rel="stylesheet" href="{{asset("IndexCss/index.Css")}}?v={{ time() }}">
         <link rel="stylesheet" href="{{asset("IndexCss/fotos.Css")}}?v={{ time() }}">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
